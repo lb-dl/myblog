4bf8678098c9f150e1b9e69a48d0042d240cbc98
lb-dl <lb_dl@i.ua>
2021-01-16 20:52:32 +0200

    search with trigram similarity

--- src/myblog/views.py
+++ src/myblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 from myblog.models import Post
 
@@ -149,8 +150,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            # search for posts with a custom SearchVector instance built with title and body fields
+
+            # search_vector = SearchVector('title', weight='A') +\
+            #                 SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #    search=search_vector, rank=SearchRank(search_vector, search_query))\
+            #    .filter(rank__gte=0.3).order_by('-rank')
+
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),).filter(search=query)
+                similarity = TrigramSimilarity('title', query),)\
+                .filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})
